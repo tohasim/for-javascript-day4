9369a29320e2c9bb52d7bf3882b957652cddfe2f
Tohasim <toreh.simonsen@live.dk>
2023-09-21 10:25:25 +0200

    Up to break

--- pages/allCars/allCars.js
+++ pages/allCars/allCars.js
@@ -1,5 +1,26 @@
+import { SERVER_URL } from "../../settings.js";
 
+export async function initAllCars() {
+	console.log("initAllCars");
 
-export function initAllCars(){
-  console.log("initAllCars")
+	const cars = await getCars();
+	const listItems = cars
+		.map((car) => `<li>${car.id}: ${car.brand}</li>`)
+		.join("");
+	document.getElementById("cars").innerHTML = listItems;
 }
+
+async function getCars() {
+	const cars = await fetch(SERVER_URL).then((res) => res.json());
+	return cars;
+}
+
+/*	fetch(SERVER_URL)
+		.then((res) => res.json())
+		.then((cars) => {
+			const listItems = cars
+				.map((car) => `<li>${car.id}: ${car.brand}</li>`)
+				.join("");
+			document.getElementById("cars").innerHTML = listItems;
+		});
+*/
